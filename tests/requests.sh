dfa0e19569f340f8fa92e9f04c938b5db22c4471
Gabriel Ramos dos Santos <gabriel.ramos@exa.com.br>
2025-02-20 14:52:25 -0300

    Adicionando arquivo de configuração e ajustando tests

--- tests/requests.sh
+++ tests/requests.sh
@@ -10,3 +10,21 @@ curl -X POST http://localhost:8080/ \
 curl -X POST http://localhost:8080/ \
   -H "Content-Type: application/json" \
   -d '{"url": "https://www.kaggle.com/api/v1/datasets/download/amandamartin62/simulated-transactions-parquet-format"}'
+
+
+#HML
+
+curl -X POST https://{URL AQUI}/ \
+  -H "Content-Type: application/json" \
+  -d '{"url": "https://www.filesampleshub.com/download/code/parquet/sample3.parquet"}'
+  
+  
+# ----| Arquivo de 7.18gb
+curl -X POST https://{URL AQUI}/ \
+  -H "Content-Type: application/json" \
+  -d '{"url": "https://www.kaggle.com/api/v1/datasets/download/amandamartin62/simulated-transactions-parquet-format"}'
+
+# ----| Arquivo de 7.18gb
+curl -X POST https://{URL AQUI}/ \
+  -H "Content-Type: application/json" \
+  -d '{"url": "https://bit.ly/10GbOVHserver"}'
